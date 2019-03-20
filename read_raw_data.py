4944d5c1f08736e8b7bb6e57118770532f4fdfcc
Lorenzo Palmieri <33118964+fintechdummy@users.noreply.github.com>
2019-03-20 16:47:05 +0000

    Modified json file path
    For protection purposes

--- read_raw_data.py
+++ read_raw_data.py
@@ -1,7 +1,7 @@
 import json
 
 # load the json file from local
-with open(f'/Users/lorenzopalmieri/Desktop/Job search/Suade/Example_Data.json', encoding='utf-8-sig') as json_file:
+with open(f'/Users/.../Example_Data.json', encoding='utf-8-sig') as json_file:
     json_data = json.load(json_file)
 
 # store the data in a variable
